eb5d58e34dedb847bf0a91c8f4db48d7eb237b2a
SwayamY <swayam.science777@gmail.com>
2025-04-04 23:43:17 +0530

    feat: version 3 - Add Redis, expiry & collision prevention

--- app/crud.py
+++ app/crud.py
@@ -1,8 +1,10 @@
+from sqlalchemy import and_, or_
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from app.models import URL
+from datetime import datetime, timezone
 
-async def create_url(db:AsyncSession, long_url:str,short_url:str) -> URL | dict:
+async def create_url(db:AsyncSession, long_url:str, short_url:str, expires_at=None) -> URL | dict:
     existing = await get_url_by_short(db,short_url)
     if existing:
         print(f"Short URL already  exists: {short_url}") # debugging
@@ -10,14 +12,21 @@ async def create_url(db:AsyncSession, long_url:str,short_url:str) -> URL | dict:
     
 
     print(f"creating short URL : {short_url} for {long_url}")
-    new_url = URL(long_url = long_url , short_url = short_url)
+    new_url = URL(long_url = long_url , short_url = short_url, expires_at = expires_at)
     db.add(new_url)
     await db.commit()
     await db.refresh(new_url)
     return new_url
 
 async def get_url_by_short(db:AsyncSession,short_url:str):
-    result = await db.execute(select(URL).filter(URL.short_url == short_url))
+    now = datetime.now(timezone.utc)
+    result = await db.execute(
+                            select(URL).where
+                              (and_
+                                (URL.short_url == short_url,
+                               or_
+                                (URL.expires_at.is_(None),
+                                 URL.expires_at > now))))
     url_entry = result.scalars().first()
     print(f"checks if short URL exists: {short_url} --> {url_entry}")
     return url_entry
@@ -37,4 +46,13 @@ async def delete_url(db:AsyncSession,short_url:str):
         return True
     print(f"short url not found : {short_url}")
     return False
+
+async def search_short_urls(db:AsyncSession, long_url: str):
+    now = datetime.now(timezone.utc)
+    result = await db.execute(select(URL).where(
+                                and_(
+                                URL.long_url == long_url,
+                                or_(URL.expires_at.is_(None),URL.expires_at > now))
+                                ))
+    return result.scalars().all()
     
--- app/models.py
+++ app/models.py
@@ -1,9 +1,13 @@
-from sqlalchemy import Column,Integer,String,Text
+from sqlalchemy import Column,Integer,String,Text , DateTime
 from app.database import Base
+from datetime import datetime , timezone
+
 
 class URL(Base):
     __tablename__ = "urls"
 
     id = Column(Integer,primary_key=True)
     long_url = Column(Text,nullable=False)
-    short_url = Column(String(10),unique=True,nullable=False)
+    short_url = Column(String(10),unique=True,index=True,nullable=False)
+    created_at = Column(DateTime(timezone=True), default= datetime.now(timezone.utc))
+    expires_at = Column(DateTime(timezone=True), nullable=True)
