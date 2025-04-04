13cb73cbc6a654d12bab02b60f70fb7a42960f3d
SwayamY <swayam.science777@gmail.com>
2025-04-04 23:43:17 +0530

    version -3 added Redis , expiry system , collision prevention

--- app/database.py
+++ app/database.py
@@ -1,5 +1,6 @@
 import os
 import asyncio
+import redis.asyncio as aioredis
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
 
@@ -11,8 +12,10 @@ load_dotenv()
 POSTGRES_USER = os.getenv("POSTGRES_USER",'sweyam')
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")  # Default to 'pgbouncer' if not set
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "url_shortener_db")  # Default to 'pgbouncer' if not set
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")  # Default to 'pgbouncer' if not set
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")  # Default to 6432
+REDIS_PORT = os.getenv("REDIS_PORT", "6379")  # Default to 6432
 
 
 # DEBUG: Print to check if variables are loaded correctly
@@ -21,6 +24,9 @@ print(f"POSTGRES_PASSWORD={POSTGRES_PASSWORD}")
 print(f"POSTGRES_DB={POSTGRES_DB}")
 print(f"POSTGRES_HOST={POSTGRES_HOST}")
 print(f"POSTGRES_PORT={POSTGRES_PORT}")
+print(f"REDIS_PORT={REDIS_PORT}")
+print(f"REDIS_HOST={REDIS_HOST}")
+
 
 DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
  
@@ -55,7 +61,25 @@ async def get_db():
     async with SessionLocal() as db:
         yield db
 
-async def test_connection():
+#redis integration
+REDIS_HOST = os.getenv("REDIS_HOST","redis")
+REDIS_PORT = int(os.getenv("REDIS_PORT",6379))
+
+async def get_redis():
+    redis = await aioredis.from_url(f"redis://{REDIS_HOST}:{REDIS_PORT}",decode_responses=True)
+    return redis
+
+async def test_redis_connection():
+    try:
+        redis = await get_redis()
+        await redis.ping()
+        print("Successful connected to redis")
+    except Exception as e:
+        print("redis connection failed")
+        print(e)
+
+
+async def test_db_connection():
     try:
         async with engine.begin() as conn:
             print("success connected to DB")
@@ -64,7 +88,8 @@ async def test_connection():
         print(e)
 
 if __name__ == "__main__":
+    asyncio.run(test_redis_connection()) # check redis connection   
     try:
-        asyncio.run(test_connection())
+        asyncio.run(test_db_connection()) # checks db:postgres connection
     except RuntimeError:
         print("async event loop already running. Skipping test conn")
--- app/main.py
+++ app/main.py
@@ -2,14 +2,21 @@ from fastapi import FastAPI, Depends ,HTTPException
 from fastapi.responses import RedirectResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 from app import crud,models,database
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
+import random
+import string
+from typing import Optional , List
+import traceback
+from datetime import datetime, timedelta, timezone
+
 
 app = FastAPI(title="URL Shortener API")
 
 
 class ShortenRequest(BaseModel):
     long_url: str
-    short_url: str
+    short_url: Optional[str] = None
+    expiry_days: Optional[int] = 30
 
 async def get_db():
     async with database.SessionLocal() as db:
@@ -23,14 +30,52 @@ async def get_db():
 async def read_root():
     return {"message": "URL Shortener API is running!"}
 
+@app.get("/healthz")
+async def health_chk():
+    return {"status":"ok"}
+
+
+
 @app.post("/shorten/")
 async def shorten_url(request:ShortenRequest,db:AsyncSession = Depends(get_db)):
-    existing = await crud.get_url_by_short(db,request.short_url)
-    if existing:
-        raise HTTPException(status_code=400, detail="SHORT URL already exists")
+    if request.expiry_days not in [5,15,30]:
+        request.expiry_days = 30 # default period
+    
+    max_attempts = 5
+    attempt = 0 
+    
+    short_url = request.short_url
 
-    new_url = await crud.create_url(db,request.long_url,request.short_url)
-    return {"id":new_url.id ,"long_url":new_url.long_url, "short_url":new_url.short_url}
+    if not short_url:
+        while attempt < max_attempts:
+            short_url = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+            existing = await crud.get_url_by_short(db,short_url)
+            if not existing:
+                break
+            attempt +=1
+            print(f"collison attempt {attempt}:'{short_url}' already exists")
+
+        if attempt == max_attempts:
+            raise HTTPException(status_code=500,detail="Failed to generate unique short url")
+    else:
+        existing = await crud.get_url_by_short(db,short_url)
+        if existing:
+            raise HTTPException(status_code=400, detail="SHORT URL already exists")
+    
+    expires_at = datetime.now(timezone.utc) + timedelta(days= request.expiry_days)
+
+    new_url = await crud.create_url(
+                                    db=db,
+                                    long_url=request.long_url,
+                                    short_url=short_url,
+                                    expires_at=expires_at
+                                    )
+    
+    return {"id":new_url.id ,
+            "long_url":new_url.long_url,
+            "short_url":new_url.short_url,
+            "expires_at":new_url.expires_at
+              }
 
 
 @app.get("/{short_url}")
@@ -46,3 +91,27 @@ async def delete_url(short_url:str , db:AsyncSession = Depends(get_db)):
     if not deleted:
         raise HTTPException(status_code=404,detail="SHORT URL NOT FOUND")
     return {"message":"Short URL deleted successfully"}
+
+@app.get("/search/")
+async def search_url(long_url:str ,db: AsyncSession = Depends(get_db)):
+    try:
+        print(f"[DEBUG] Searching short URLs for: {long_url}")  # Debug line
+        urls = await crud.search_short_urls(db, long_url)
+        if not urls:
+            return {
+                "message":"",
+                "suggested_action": {
+                   "method": "POST",
+                   "url" :"/shorten/",
+                   "body" : {
+                       
+                   "long_url": long_url
+                   }
+                }
+            }
+
+        return [{"short_url": url.short_url,"expires_at": url.expires_at} for url in urls]
+    except Exception as e:
+        print("error => ",e)
+        traceback.print_exc()
+        raise HTTPException(status_code=500,detail="Somewthing went wrong")
