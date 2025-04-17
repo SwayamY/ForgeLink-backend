fd4e6d392cc1cacc5d6f6ca2b966be65b7ce6989
SwayamY <swayam.science777@gmail.com>
2025-04-18 01:12:16 +0530

    fixed cors credentials issue -- just removed it

--- app/main.py
+++ app/main.py
@@ -42,12 +42,12 @@ class ModeRequest(BaseModel):
 
 
 # CORS 
-origins = ["https://forgelink.netlify.app"]
+origins = ["https://forgelink.netlify.app","https://linkforge.duckdns.org"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
