e7ac793e6856ee402d8f64527f5e623be784b328
SwayamY <swayam.science777@gmail.com>
2025-04-25 16:51:14 +0530

    fix: removed credentials from request schema

--- app/main.py
+++ app/main.py
@@ -49,7 +49,7 @@ origins = ["https://forgelink.netlify.app"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
